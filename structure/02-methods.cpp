e498da79c4b79d446a9177bf4795c2e23aa67655
ut-kr <utkarsh@cyberfear.com>
2022-06-02 23:32:45 +1000

    ✨ update restructure

--- structure/02-methods.cpp
+++ structure/02-methods.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-using namespace std;
 
 void defined_and_declared() {
-    cout << "the typical way to define functions" << endl;
+    std::cout << "the typical way to define functions" << std::endl;
 }
 
 // declaring function prototype; better coding practice
@@ -17,18 +16,18 @@ int main() {
     declare_first2("John", 21, true);
     declare_first2("Alex", 36);
     
-    cout << "sum of 2 and 4 is: " << declare_first3(2, 4) << endl;
+    std::cout << "sum of 2 and 4 is: " << declare_first3(2, 4) << std::endl;
     return 0;
 }
 
 void declare_first1() {
-    cout << "functions can be declared first and defined later like this one" << endl;
+    std::cout << "calling declared prototype" << std::endl;
 }
 
 //? parameters type is also defined. default value can be assigned if nothing is passed
 void declare_first2(string name, int age, bool from_army) {
-    cout << name << " is " << age << " old." << endl
-    << "in army: "<< from_army << endl;
+    std::cout << name << " is " << age << " old." << std::endl
+    << "in army: "<< from_army << std::endl;
 }
 
 int declare_first3(int num1, int num2) {
