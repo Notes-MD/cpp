c94814dea6755fc3782f208e2ed7cea84b6c8cdc
ut-kr <utkarsh@cyberfear.com>
2022-06-03 20:28:05 +1000

    ✨ update restructure

--- structure/10-pointers.cpp
+++ structure/10-pointers.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 
 int main() {
-  // pointer is a variable whose value is the address of another variable. 
-  // type *var-identifier;
-
-  int *int_pointer;
-  float *float_pointer;
-  char *char_pointer;
-
-  // even though these pointer types are for different datatypes, they are all of same datatype
-  // which is a a long hexadecimal number.
-  // difference only exists between the variables these pointers refer to.
-
-  // example:
-  int var = 32;
-  int *id;
-
-  id = var;
-  std::cout << var << std::endl;  // would print 32
-  std::cout << id << std::endl;	  // would print adress of var
-  std::cout << *id << std::endl;  // would print 32
-
+  //?====[ References (basically same as bash aliases) ]===?
+  std::string food1 = "Pizza";
+  std::string &meal = food1;
+  std::cout << food1 << "\n";    // output --> Pizza
+  std::cout << meal << "\n";     // output --> Pizza
+  
+  //?====[ Memory addresses ]=============================?
+  std::string food2 = "Pizza";
+  std::cout << &food2;           // output --> 0x6dfed4
+  
+  //?====[ Pointers ]=====================================?
+  std::string food3 = "Pizza";   // A food variable of type string
+  std::string* ptr3 = &food3;    // A pointer variable, with the name ptr, that stores the address of food
+  std::cout << food3 << "\n";    // Output the value of food (Pizza)
+  std::cout << &food3 << "\n";   // Output the memory address of food (0x6dfed4)
+  std::cout << ptr3 << "\n";     // Output the memory address of food with the pointer (0x6dfed4)
+  
+  //? changing ptr also changes food but location stays constant
+  std::string food4 = "Pizza";
+  std::string* ptr4 = &food4;
+  std::cout << food4 << "\n";    // Output the value of food (Pizza)
+  std::cout << &food4 << "\n";   // Output the memory address of food (0x6dfed4)
+  std::cout << *ptr4 << "\n";    // Access the memory address of food and output its value (Pizza)
+  *ptr4 = "Hamburger";           // Change the value of the pointer
+  std::cout << *ptr4 << "\n";    // Output the new value of the pointer (Hamburger)
+  std::cout << food4 << "\n";    // Output the new value of the food variable (Hamburger)
+  std::cout << &food4 << "\n";   // location stays the same
+  
   // pointers can be defined values during declaration, however this value would be some memory
   // address hence hex value. For example:
   // int *var2 = 232; would give an error as 232 isn't hexadecimal number, whereas-
