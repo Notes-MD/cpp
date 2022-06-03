9c0b7d327b71b12c811dae9888ee4d62c578cd58
ut-kr <utkarsh@cyberfear.com>
2022-06-03 20:01:04 +1000

    🔧 fixed std::

--- structure/06-oop_inheritance.cpp
+++ structure/06-oop_inheritance.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
-using namespace std;
 
 class ParentClass {
     public:
-        string public_string = "public readable n writable data";
+        std::string public_string = "public readable n writable data";
         void hello_message() {
-            cout << "hello world\n";
+            std::cout << "hello world\n";
         }
     protected:
-        string info = "secret between classes";
+        std::string info = "secret between classes";
 };
 
 class ChildClass : public ParentClass {
     public:
-        string public_string = "edited the content";
+        std::string public_string = "edited the content";
         ChildClass(){
             // child class is exposing the secret message
-            cout << info << endl;
+            std::cout << info << std::endl;
         }
 };
 
@@ -24,7 +23,7 @@ class AnotherParentClass {
     public:
         int random_numbers = 324234;
         void tell_bye() {
-            cout << "bye world" << endl;
+            std::cout << "bye world" << std::endl;
         }
 };
 
@@ -36,10 +35,10 @@ class MultipleParents : public AnotherParentClass, public ParentClass {
 int main(){
     ChildClass test_object;
     test_object.hello_message();
-    cout << test_object.public_string << endl;
-    cout << endl;
+    std::cout << test_object.public_string << std::endl;
+    std::cout << std::endl;
 
     MultipleParents text_object2;
-    cout << text_object2.random_numbers << endl;
+    std::cout << text_object2.random_numbers << std::endl;
     text_object2.hello_message();
 }
